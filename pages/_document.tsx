774cba5bf3279e8b9218444896617f46f3b93318
evan-rosa <evandanrosa@gmail.com>
2022-09-05 15:12:01 -0400

    GTM serverside

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,44 @@ class WebDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@400;600&family=Source+Code+Pro:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
+          <Script async src="https://gtm.evro.dev/gtag/js?id=G-H9MLDVMFBC"></Script>
 
+          <Script
+            defer
+            id="gtm_script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'TAG_ID');`,
+            }}
+          ></Script>
+          <Script
+            defer
+            id="gtm_script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://gtm.evro.dev/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NL3W835');`,
+            }}
+          ></Script>
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe
+              src="https://gtm.evro.dev/ns.html?id=GTM-NL3W835"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
