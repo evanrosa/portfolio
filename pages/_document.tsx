44f6b7be3a785addbfbecd1eabecd50ab489c016
evan-rosa <evandanrosa@gmail.com>
2022-09-05 14:43:56 -0400

    RIP client side GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,30 +12,9 @@ class WebDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@400;600&family=Source+Code+Pro:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
-          <Script
-            defer
-            id="gtm_script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NL3W835');`,
-            }}
-          ></Script>
+
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NL3W835"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>`,
-            }}
-          ></noscript>
           <Main />
           <NextScript />
         </body>
